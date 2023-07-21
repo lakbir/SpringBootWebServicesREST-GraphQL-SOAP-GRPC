1ef65f5514e2070d7ead7ff4a73206bba8028d31
lakbir <lakbir1993@gmail.com>
2023-07-22 01:01:02 +0200

    Partie 2

--- src/main/java/customer/service/customerservice/mapper/CustomerMapper.java
+++ src/main/java/customer/service/customerservice/mapper/CustomerMapper.java
@@ -2,15 +2,22 @@ package customer.service.customerservice.mapper;
 
 import customer.service.customerservice.dto.CustomerRequest;
 import customer.service.customerservice.entities.Customer;
+import customer.service.customerservice.stub.CustomerServiceOuterClass;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerMapper {
+    ModelMapper modelMapper = new ModelMapper();
     public Customer from(CustomerRequest customerRequest){
-        ModelMapper modelMapper = new ModelMapper();
-        Customer customer = new Customer();
-        Customer customer1 = modelMapper.map(customerRequest, Customer.class);
-        return customer1;
+        return modelMapper.map(customerRequest, Customer.class);
+    }
+
+    public CustomerServiceOuterClass.Customer fromCustomer(Customer customer){
+        return modelMapper.map(customer, CustomerServiceOuterClass.Customer.Builder.class).build();
+    }
+
+    public Customer fromGrpcCustomerRequest(CustomerServiceOuterClass.CustomerRequest customerRequest){
+        return modelMapper.map(customerRequest, Customer.class);
     }
 }
